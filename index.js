4179cb7f12756424a417df9b5d9999f1a5ee1120
vsapiens <erickfrank@icloud.com>
2020-01-27 22:18:32 -0600

    Added website

---
+++ index.js
@@ -0,0 +1,51 @@
+let express = require("express");
+let morgan = require("morgan");
+let bodyParser = require("body-parser");
+let jsonParser = bodyParser.json();
+let mongoose = require("mongoose");
+
+let app = express();
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+/*
+let server;
+function runServer(port, databaseUrl) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, response => {
+      if (response) {
+        return reject(response);
+      } else {
+        server = app
+          .listen(port, () => {
+            console.log("App is running on port " + port);
+            resolve();
+          })
+          .on("error", err => {
+            mongoose.disconnect();
+            return reject(err);
+          });
+      }
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log("Closing the server");
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  });
+}
+
+runServer(PORT, DATABASE_URL);
+
+module.exports = { app, runServer, closeServer };
+*/
