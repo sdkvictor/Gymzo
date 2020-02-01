fe2466dec55a4e349ff2363adc4734fc505e3fe6
Victor <sdkvictor@github.com>
2020-02-01 13:20:53 -0600

    react login start

---
+++ ReactGymzo/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Dashboard = () => {
+    return (
+        <di>
+            <div>
+                <h1>Dashboard</h1>
+                <h1>Dashboard</h1>
+            </div>
+        </di>
+    );
+}
+export default Dashboard;
